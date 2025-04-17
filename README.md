0f7a159fe4f727dcbdb870d533c38bb555061101
tccuong8 <chicuong04@gmail.com>
2025-04-17 15:30:16 -0500

    database tutorial

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Financial-Advisor
+
+Base tutorial: https://testdriven.io/blog/developing-a-single-page-app-with-fastapi-and-vuejs/
+
+Now, when you make changes to the models, you can run the following commands to update the database:
+
+$ docker-compose exec backend aerich migrate
+$ docker-compose exec backend aerich upgrade
+
+To run the app:
+npm run serve        
--- fastapi-vue/services/backend/src/main.py
+++ fastapi-vue/services/backend/src/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware  # NEW
+from fastapi.middleware.cors import CORSMiddleware
+from tortoise import Tortoise  # NEW
 
+from src.database.register import register_tortoise
+from src.database.config import TORTOISE_ORM
+
+
+# enable schemas to read relationship between models
+Tortoise.init_models(["src.database.models"], "models")  # NEW
 
 app = FastAPI()
 
-# NEW
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:8080"],
@@ -12,6 +18,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
 
 
 @app.get("/")
