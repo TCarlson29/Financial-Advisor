e84f0aa2c6c66e8a5f45ad054f12cd0d2fbc1efb
tccuong8 <chicuong04@gmail.com>
2025-04-20 18:15:19 -0500

    Test brining out todo sample project

--- app/database_sample.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-DATABASE_url = "sqlite:///./sql_app.db"
-engine = create_engine(DATABASE_url, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
